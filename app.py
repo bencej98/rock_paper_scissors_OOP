718a614b174726ff43ed7b7295e8c8890b90fa29
bencejenei <bencej9812@gmail.com>
2023-04-22 20:26:51 +0200

    Added store_player_move method

--- app.py
+++ app.py
@@ -4,4 +4,17 @@ class Player():
         self.score = 0
         self.name = name
 
+    def store_player_move(self):
+        valid_choices = ["rock", "paper", "scissors"]
+        while True:
+            player_move = input(f"{self.name}, please choose your next move: ")
+            if player_move.lower() not in valid_choices:
+                print("You can only choose from Rock, Paper or Scissors")
+                continue
+            else:
+                print(player_move)
+                break
 
+
+test = Player("Bence")
+test.store_player_move()
