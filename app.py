3158b8a1dc85ed7fd13689f1f2110b6d37e3fa26
bencejenei <bencej9812@gmail.com>
2023-04-22 21:04:41 +0200

    Create Computer class and store_move function for it

--- app.py
+++ app.py
@@ -1,21 +1,35 @@
+import random
+
 class Player():
     def __init__(self, name) -> None:
         self.choice = None
         self.score = 0
         self.name = name
+        self.valid_choices = ["rock", "paper", "scissors"]
 
-    def store_player_move(self):
-        valid_choices = ["rock", "paper", "scissors"]
+    def store_move(self) -> None:
         while True:
-            player_move = input(f"{self.name}, please choose your next move: ")
-            if player_move.lower() not in valid_choices:
+            self.choice = input(f"{self.name}, please choose your next move: ")
+            if self.choice.lower() not in self.valid_choices:
                 print("You can only choose from Rock, Paper or Scissors")
                 continue
             else:
-                print(player_move)
+                print(self.choice)
                 break
-    def increase_score(self):
+            
+    def increase_score(self) -> None:
         self.score += 1
+
+class Computer(Player):
+    def __init__(self, name) -> None:
+        super().__init__(name)
+        self.name = "Computer"
+
+    def store_move(self) -> None:
+        self.choice = random.choice(self.valid_choices)
+        
+
+
 
 
 test = Player("Bence")
