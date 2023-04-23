27c650973289023c571317a854d04ca067862793
bencejenei <bencej9812@gmail.com>
2023-04-23 16:57:13 +0200

    Added test for game_over function

--- test_app.py
+++ test_app.py
@@ -9,8 +9,8 @@ from io import StringIO
 class TestRockPaperScissors(unittest.TestCase):
     def setUp(self) -> None: 
         self.player = Player("player")
-        self.computer_player = Computer()
-        self.current_game = GameManager(self.player, self.computer_player)
+        self.computer = Computer()
+        self.current_game = GameManager(self.player, self.computer)
 
     def test_if_computer_choice_is_valid(self):
         self.current_game.computer.get_move()
@@ -24,6 +24,17 @@ class TestRockPaperScissors(unittest.TestCase):
     @patch('sys.stdout', new_callable=StringIO)
     def test_decide_the_winner(self, mock_stdout):
         self.player.choice = 'rock'
-        self.computer_player.choice = 'scissors'
+        self.computer.choice = 'scissors'
         self.current_game.decide_the_winner()
         self.assertEqual(mock_stdout.getvalue(), f'{self.player.name} won the round!\n')
+
+    def test_check_game_over_if_player_has_3_points(self):
+        self.player.score = 3
+        self.current_game.check_the_score()
+        self.assertTrue(self.current_game.is_game_over)
+
+    
+    def test_check_game_over_if_computer_has_3_points(self):
+        self.computer.score = 3
+        self.current_game.check_the_score()
+        self.assertTrue(self.current_game.is_game_over)
