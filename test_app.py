6f9f97af9682ba525f5648ab17a0fe3e67d404cd
bencejenei <bencej9812@gmail.com>
2023-04-23 16:46:35 +0200

    Add test for get_move function

--- test_app.py
+++ test_app.py
@@ -2,6 +2,8 @@ import unittest
 import sys
 sys.path.append("C:\home\bencejenei\rock_paper_scissors_OOP")
 from app import Player, Computer, GameManager
+from unittest.mock import patch
+from io import StringIO
 
 
 class TestRockPaperScissors(unittest.TestCase):
@@ -13,3 +15,8 @@ class TestRockPaperScissors(unittest.TestCase):
     def test_if_computer_choice_is_valid(self):
         self.current_game.computer.get_move()
         self.assertIn(self.current_game.computer.choice, self.current_game.computer.valid_choices)
+
+    def test_get_move_of_the_player(self):
+        with patch('builtins.input', side_effect=['rock']):
+            self.player.get_move()
+            self.assertEqual(self.player.choice, 'rock')
