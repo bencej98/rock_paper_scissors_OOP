ff1952dc525ba64ce3237e418d9d90735ce767b3
bencejenei <bencej9812@gmail.com>
2023-04-23 15:42:02 +0200

    Added docstring for some of the functions

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ class Player():
         self.valid_choices = ["rock", "paper", "scissors"]
 
     def get_move(self) -> None:
+        """Gets the current move of the player"""
         while True:
             self.choice = input(f"{self.name}, please choose your next choice: ").lower()
             if self.choice not in self.valid_choices:
@@ -34,6 +35,7 @@ class GameManager():
         self.is_game_over = False
 
     def decide_the_winner(self) -> None:
+        """Based on the player's and computer's choice decides the winner of the current round"""
         if self.player.choice == self.computer.choice:
             print("It's a tie")
         elif (self.player.choice == "rock" and self.computer.choice == "scissors") or \
@@ -46,6 +48,7 @@ class GameManager():
             print(f"{self.computer.name} won the round!")
 
     def check_the_score(self) -> None:
+        """After every round checks the current score of each player"""
         if self.player.score == 3:
             print(f"{self.player.name} wins the game! Congrats!")
             self.is_game_over = True
@@ -54,6 +57,7 @@ class GameManager():
             self.is_game_over = True
 
     def start_game(self) -> None:
+        """Manages the game with all the related functions"""
         print("Prepare yourself! The game against the computer has started!")
         while self.is_game_over is False:
             self.player.get_move()
